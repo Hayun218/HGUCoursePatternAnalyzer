029a66b3f5d216211ca5fb9d70b66d521f1c3af2
Hayun218 <21800325@handong.edu>
2019-04-25 16:27:52 +0900

    Create Course.java file

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,15 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+	private String courseName;
+	
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public Course(String name) {
+		courseName = name;
+	}
+
+}
